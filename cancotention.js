6f1886aebf9284ebe2782aad6ae51e39c9823c22
RimshakanwalArin <160338555+RimshakanwalArin@users.noreply.github.com>
2024-03-25 14:10:03 +0500

    Add files via upload

---
+++ cancotention.js
@@ -0,0 +1,13 @@
+// concotention
+// let num1 = `1`;  //string
+// let num2 = 1;    //number
+// let sum =  num1+num2
+// console.log(sum);
+// addition
+var num3 = 1;
+var num4 = 1;
+var sum1 = num3 + num4;
+console.log(sum1);
+console.log("the num3 num4 wili be", sum1); //tempale back tick
+//variable declear cullre breaket 
+console.log("the sum of ".concat(num3, " and ").concat(num4, "will be ").concat(sum1));
---
+++ cancotention.ts
@@ -0,0 +1,17 @@
+// concotention
+
+// let num1 = `1`;  //string
+// let num2 = 1;    //number
+// let sum =  num1+num2
+// console.log(sum);
+
+// addition
+let num3 = 1;
+let num4 = 1;
+let sum1 = num3+num4;
+console.log(sum1);
+console.log(`the num3 num4 wili be`, sum1); //tempale back tick
+
+//variable declear cullre breaket 
+
+console.log(`the sum of ${num3} and ${num4} will be ${sum1}`);
